aaf940d17ff9af7948a6d51e5ce2df95a051e2bb
mohammed alsamaki <86600688+mohammedalsamki@users.noreply.github.com>
2021-08-10 23:25:29 +0300

    add form

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 'use strict';
 
-let allOfPro = document.getElementById('container');
+let salesid = document.getElementById('container');
+
+let tabElementmain = document.createElement('table');
+salesid.appendChild(tabElementmain);
+
+let trElementmain = document.createElement('tr');
+tabElementmain.appendChild(trElementmain);
 
 let time = ['6:00 AM', '7:00 AM', '8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM', '6:00 PM', '7:00 PM'];
 
-let cookiesArrTotal = [];
+let allOdCookes = [];
 
-let arrayOfOpject = [];
+let arrOfObj = [];
 
 function StoreLocation(minPH, maxPH, city, cookesAvg) {
   this.minPH = minPH;
@@ -15,7 +21,7 @@ function StoreLocation(minPH, maxPH, city, cookesAvg) {
   this.coockesTotal = 0;
   this.cookesAvg = cookesAvg;
   this.sumOfTotal = [];
-  arrayOfOpject.push(this);
+  arrOfObj.push(this);
 }
 
 StoreLocation.prototype.ranNum = function (min, max) {
@@ -34,104 +40,96 @@ StoreLocation.prototype.finalREsalt = function () {
   return this.sumOfTotal;
 };
 
-
 StoreLocation.prototype.render = function () {
-  let secEL = document.createElement('section');
-  allOfPro.appendChild(secEL);
 
-  let tabEl = document.createElement('table');
-  secEL.appendChild(tabEl);
+  let tr1Elementmain = document.createElement('tr');
+  tabElementmain.appendChild(tr1Elementmain);
 
-  let trEl = document.createElement('tr');
-  tabEl.appendChild(trEl);
-
-
-  let thEl = document.createElement('th');
-  trEl.appendChild(thEl);
-  thEl.textContent = this.city;
-
+  let thElement1main = document.createElement('th');
+  thElement1main.textContent = this.locationName;
+  tr1Elementmain.appendChild(thElement1main);
 
   for (let i = 0; i < this.sumOfTotal.length; i++) {
-    let tdEl2 = document.createElement('td');
-    trEl.appendChild(tdEl2);
-    tdEl2.textContent = [this.sumOfTotal[i]];
+    let tdElement2main = document.createElement('td');
+    tdElement2main.textContent = this.sumOfTotal[i];
+    tr1Elementmain.appendChild(tdElement2main);
   }
-  let tdEl3 = document.createElement('td');
-  tdEl3.textContent = this.coockesTotal;
-  trEl.appendChild(tdEl3);
-  cookiesArrTotal.push (this.coockesTotal)[0];
+  let tdElement2main = document.createElement('td');
+  tdElement2main.textContent = this.coockesTotal;
+  tr1Elementmain.appendChild(tdElement2main);
+  allOdCookes.push(this.coockesTotal)[0];
 };
 
-
 let seattle = new StoreLocation(23, 65, 'Seattle', 6.3, []);
-
 let tokyo = new StoreLocation(3, 24, 'Tokyo', 1.2, []);
-
 let dubai = new StoreLocation(11, 38, 'Dubai', 3.7, []);
-
 let paris = new StoreLocation(20, 38, 'Paris', 2.3, []);
-
 let lima = new StoreLocation(2, 16, 'Lima', 4.6, []);
 
-let cityNew = [seattle, tokyo, dubai, paris, lima];
+let StoreLocationArr = [seattle, tokyo, dubai, paris, lima];
 
-for (let i = 0; i < cityNew.length; i++) {
-  cityNew[i].finalREsalt();
-  cityNew[i].render();
+for (let i = 0; i < StoreLocationArr.length; i++) {
+  StoreLocationArr[i].finalREsalt();
+  StoreLocationArr[i].render();
 }
 
-function topTable() {
-  let tobnew = document.getElementById('tableTop');
-
-  let tableTop = document.createElement('table');
-  tobnew.appendChild(tableTop);
-  let trTop = document.createElement('tr');
-  tableTop.appendChild(trTop);
+function tableTop() {
   time.unshift('');
   time.push('Total of daily total');
   for (let i = 0; i < time.length; i++) {
-    let th0El = document.createElement('th');
-    trTop.appendChild(th0El);
-    th0El.textContent = [time[i]];
+    let th1Elementmain = document.createElement('th');
+    th1Elementmain.textContent = time[i];
+    trElementmain.appendChild(th1Elementmain);
   }
   time.shift();
   time.pop();
 }
 
-topTable();
-
+tableTop();
 
 function tableBottom() {
-  let bottom = document.getElementById('tableBottom');
 
-  let bottomElement = document.createElement('table');
-  bottom.appendChild(bottomElement);
-  let trBottom = document.createElement('tr');
-  bottomElement.appendChild(trBottom);
+  let trBottomlementmain = document.createElement('tr');
+  tabElementmain.appendChild(trBottomlementmain);
 
-  let totalEl = document.createElement('th');
-  trBottom.appendChild(totalEl);
-  totalEl.textContent = 'total';
+  let totalElementmain = document.createElement('th');
+  totalElementmain.textContent = 'total';
+  trBottomlementmain.appendChild(totalElementmain);
 
-
-  for(let i = 0; i < time.length; i++){
-    let colomTotal = 0;
-    for(let m = 0; m < arrayOfOpject.length; m++){
-      colomTotal += arrayOfOpject[m].sumOfTotal[i];
+  for (let i = 0; i < time.length; i++) {
+    let totalColommain = 0;
+    for (let m = 0; m < arrOfObj.length; m++) {
+      totalColommain += arrOfObj[m].sumOfTotal[i];
     }
-    let th2El = document.createElement('th');
-    trBottom.appendChild(th2El);
-    th2El.textContent = colomTotal;
+    let td2Elementmain = document.createElement('td');
+    td2Elementmain.textContent = totalColommain;
+    trBottomlementmain.appendChild(td2Elementmain);
   }
 
-  let totalValu = cookiesArrTotal;
-  let sumtotal = totalValu.reduce(function(sumation, valuejust)
-  { return sumation + valuejust; }, 0);
+  let valueOfTotalmain = allOdCookes;
+  let totalmain = valueOfTotalmain.reduce(function (accum, currVal) { return accum + currVal; }, 0);
 
-  let finallTotal = document.createElement('th');
-  trBottom.appendChild(finallTotal);
-  finallTotal.textContent = sumtotal;
+  let totalOfTotalmain = document.createElement('td');
+  totalOfTotalmain.textContent = totalmain;
+  trBottomlementmain.appendChild(totalOfTotalmain);
 }
 
 tableBottom();
+
+let cityForm = document.getElementById('locationForm');
+cityForm.addEventListener('submit', formData);
+
+function formData(event) {
+  event.preventDefault();
+  let cityName = event.target.nameOflocation.value;
+  let minNew = parseInt(event.target.minCustomersNumber.value);
+  let maxnew = parseInt(event.target.maxCustomersNumber.value);
+  let cookiesAveragenew = parseInt(event.target.cookiesAverage.value);
+  let newCity = new StoreLocation(minNew, maxnew, cityName, cookiesAveragenew, []);
+  newCity.finalREsalt();
+  newCity.render();
+  tabElementmain.deleteRow(arrOfObj.length);
+  tableBottom();
+  cityForm.reset();
+}
 
